0b00f05db6feb834e1ead51857e70d0455c024e0
Meaj <kevin.omd@gmail.com>
2018-07-13 03:42:18 -0500

    Added options to problem1Expanded.c
    * Added remaining modes to problem1Expanded.c
    * Updated README.md for Problem1 directory

--- C/Problem1/README.md
+++ C/Problem1/README.md
@@ -5,4 +5,4 @@ My implementations of solutions to [problem 1](https://projecteuler.net/problem=
 
 ### Contents
 * ***problem1.c*** - A solution to the problem as asked
-* ***problem1Expanded.c*** - An expanded solution that has 4 different modes. Running with no arguments returns the solution as asked in problem 1. Running with one argument retruns the same solution with a range equal to the argument provided.
+* ***problem1Expanded.c*** - An expanded solution that has 4 different modes. Running with no arguments returns the solution as asked in problem 1. Running with one argument retruns the same solution with a range equal to the argument provided. Running with two arguments uses user defined multiples instead of 3 and 5. Running with three arguments uses user defined multiples and user defined range (Assumes third input is the range).
--- C/Problem1/problem1Expanded.c
+++ C/Problem1/problem1Expanded.c
@@ -8,33 +8,62 @@
 #include <stdlib.h>
 
 int main(int argc, char * argv[]){
-	int max = 0;
-	unsigned long int i = 0, sum = 0;
+	int i = 0, lo_div = 0 , hi_div = 0, max = 0;
+	unsigned long int sum = 0;
 	// If no input is provided, the program runs in standard mode, with divisors 3 and 5, and range 1000
 	if (argc == 1){
 		printf("Running in standard mode:\n\n");
 		max = 1000;
+		lo_div = 3;
+		hi_div = 5;
 	}
 	// If one input is provided, variable range mode is activated, where the max is set by the user
 	if (argc == 2){
 		printf("Running in variable range mode:\n\n");
 		max = atoi(argv[1]);
+		lo_div = 3;
+		hi_div = 5;
+	}
+	// If two inputs are provided, user defined divisors are used
+	// Allows divisors to be entered in any order
+	if (argc == 3){
+		lo_div = atoi(argv[1]);
+		hi_div = atoi(argv[2]);
+		if (lo_div > hi_div){
+			int t;
+			t = hi_div;
+			hi_div = lo_div;
+			lo_div = t;
+		}
+	}
+	// If 3 inputs are provided, user defined divisors and range are used
+	// Assumes range is always provided last
+	if (argc == 4){
+		lo_div = atoi(argv[1]);
+		hi_div = atoi(argv[2]);
+		max = atoi(argv[3]);
+		if (lo_div > hi_div){
+			int t;
+			t = hi_div;
+			hi_div = lo_div;
+			lo_div = t;
+		}
 	}
 	
 	// Runs the loop from the lowest of the two divisors to max
-	for (i = 3; i < max; i++){
+	for (i = lo_div; i < max; i++){
 		// If the number is divisible by both divisors we don't want to double count
-		if (i % 3 == 0 && i % 5 == 0){
+		if (i % lo_div == 0 && i % hi_div == 0){
 			sum += i;
 		}
-		else if (i % 3 == 0){
+		else if (i % lo_div == 0){
 			sum += i;
 		}
-		else if (i % 5 == 0){
+		else if (i % hi_div == 0){
 			sum += i;
 		}
 	}
 	
-	printf("The sum of all the multiples of 3 or 5 below %d is: %lu\n", max, sum);
+	printf("The sum of all the multiples of %d or %d below %d is: %lu\n",lo_div, hi_div, max, sum);
 	
 }
