99231664938b9c0b8a5cc743277b40f05d6dfbf2
Meaj <kevin.omd@gmail.com>
2018-07-19 23:32:49 -0500

    Updated proj_euler_004.py
    * Redid algorithim in proj_euler_004.py to properly solve problem 4
    * Created iterative_palindrome function

--- Python/proj_euler_004.py
+++ Python/proj_euler_004.py
@@ -1,10 +1,24 @@
-import math
+'''
+This program will find the largest palindrome formed by three digit numbers
+By Kevin Moore
+'''
+
+'''
+Iterative Palindrome Over Range function
+Returns a list of palindromes fromed by all values in the range int_a to int_b
+'''
+def iterative_palindromes(int_a, int_b):
+    palindromes = []
+    # iterate backwards through the range from int_a to int_b
+    for i in range(int_b, int_a, -1):
+        for j in range(int_b, int_a, -1):
+            x = i * j
+            # if the string version of x is the same forwards as backwards, add it to the palindrome list
+            if str(x) == str(x)[::-1]:
+                palindromes.append(x)
+    return palindromes
 
 def main():
-    for i in range(999,101,-1):
-        for j in range(999,101,-1):
-            x = str(i*j)
-            if x == x[::-1]:
-                print(x)
+    print(max(iterative_palindromes(101,999)))
 
 main()
