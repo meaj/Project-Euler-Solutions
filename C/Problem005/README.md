d713e38f4ebfde0dd38b69c0b2ea95700fe0fb3c
Meaj <kevin.omd@gmail.com>
2018-09-18 17:09:30 -0500

    Modified C\Problem005\problem5.c
    * Fixed bugs in C\Problem005\problem5.c, now works as expected
    * Modified C\Problem005\problem5.c to use arithmetic right shifts for division by 2
    * Added README.md for this directory

---
+++ C/Problem005/README.md
@@ -0,0 +1,7 @@
+# Problem 5
+
+### Description
+My implementation of a solution to [problem 5](https://projecteuler.net/problem=5) from projecteuler.net
+
+### Contents
+* ***problem5.c*** - A solution to the problem as asked.
--- C/Problem005/problem5.c
+++ C/Problem005/problem5.c
@@ -5,44 +5,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
+// Calculates the greatest common divisor of two non-negative numbers iVal1 and iVal2
 int binaryGCD(int iVal1, int iVal2){
     int iExponent = 0;
 
     // Divide out all possible values of 2 and increase exponent value
-    while (iVal1 % 2 == 0 && iVal2 % 2 == 0){
-        iVal1 /= 2;
-        iVal2 /= 3;
+    while (!(iVal1 & 1) && !(iVal2 & 1)){
+        iVal1 >>= 1;
+        iVal2 >>= 1;
         iExponent++;
     }
-    while (iVal1 != iVal2){
+    while (iVal1 != iVal2) {
         // If the first value is even, divide by 2
-        if (iVal1 % 2 == 0){
-            iVal1 /= 2;
+        if (!(iVal1 & 1)) {
+            iVal1 >>= 1;
         }
-        // If the second value is even
-        else if (iVal2 % 2 == 0){
-            iVal2 /= 2;
+            // If the second value is even
+        else if (!(iVal2 & 1)) {
+            iVal2 >>= 1;
         }
-        // If the first value is larger, make it even
-        else if (iVal1 > iVal2){
-            iVal1 = (iVal1 - iVal2) / 2;
+            // If the first value is larger, make it even
+        else if (iVal1 > iVal2) {
+            iVal1 = (iVal1 - iVal2) >> 1;
         }
-        // If the second value is larger make it even
-        else{
-            iVal2 = (iVal2 = iVal1) / 2;
+            // If the second value is larger make it even
+        else {
+            iVal2 = (iVal2 - iVal1) >> 1;
         }
     }
-    return (iVal1 * pow(2,iExponent));
+    return (iVal1 << iExponent);
 
 }
 
+// Uses an iterative method to calculate the lcm for a range of values
 int iterativeLCM( int iVal1, int iVal2){
     int iLCM = iVal1;
     int i;
 
-    for (i = iVal1; i <= iVal2; i++){
+    for (i = iVal1; i < iVal2; i++){
         iLCM = iLCM * i / binaryGCD(i, iLCM);
     }
     return iLCM;
@@ -50,5 +51,5 @@ int iterativeLCM( int iVal1, int iVal2){
 
 int main(int argc, char * argv[]){
     int iAns = iterativeLCM(1, 20);
-    printf("%d\n", iAns);
+    printf("The smallest number divisible by the numbers 1 to 20 is: %d\n", iAns);
 }
